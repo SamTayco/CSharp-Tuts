92fedd8e43252df212b0b33d89985553a09ae3c9
Hossam <Hossam@SamTayco>
2017-08-25 11:36:20 +0200

    Variable Test
    A C# variable test

---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int variable = 4;
+            Console.Write("(0)", variable);
+        }
+    }
+}
